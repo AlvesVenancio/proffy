8db9ab23b5fdb9c19eb475e24184fd01c18f691e
VenancioFelipe <phelipeatr@gmail.com>
2020-08-06 08:13:08 -0300

    techearItem component

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,35 +2,57 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-const TeacherItem = () => {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    const createNewConnection = () => {
+        api.post('connections', {
+            user_id: teacher.id
+        });
+    }
 
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/49986077?s=460&u=6e3dcd293025aae55ee0c0fc830155e8dc9a7144&v=4" alt="Luiz Felipe" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Luiz Felipe</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    href={`https://wa.me/${teacher.whatsapp}`} 
+                    target="blank"
+                    onClick={createNewConnection}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
